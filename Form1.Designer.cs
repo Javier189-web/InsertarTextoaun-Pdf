4fd4987f6c2856c40c33bd15156510ef166c34cb
Javier189-web <1180416@alumno.um.edu.mx>
2021-10-01 12:55:56 -0500

    insertar texto a pdf

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace crearPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(100, 106);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(519, 167);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rectangle = iTextSharp.text.Rectangle;
+
+namespace crearPDF
+{
+    public partial class Form1 : Form
+    {
+        string pathPDF = @"C:\Users\unive\Desktop\FITEC\sistemasoperativos\PDF\original.pdf";
+        string pathPDF2 = @"C:\Users\unive\Desktop\FITEC\sistemasoperativos\PDF\con_texto.pdf";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Texto;
+        public void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Texto = richTextBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Objeto para leer el pdf original
+            PdfReader oReader = new PdfReader(pathPDF);
+            //Objeto que tiene el tamaño de nuestro documento
+            Rectangle oSize = oReader.GetPageSizeWithRotation(1);
+            //documento de itextsharp para realizar el trabajo asignandole el tamaño del original
+            Document oDocument = new Document(oSize);
+
+            // Creamos el objeto en el cual haremos la inserción
+            FileStream oFS = new FileStream(pathPDF2, FileMode.Create, FileAccess.Write);
+            PdfWriter oWriter = PdfWriter.GetInstance(oDocument, oFS);
+            oDocument.Open();
+
+            //El contenido del pdf, aqui se hace la escritura del contenido
+            PdfContentByte oPDF = oWriter.DirectContent;
+
+            //Propiedades de nuestra fuente a insertar
+            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            oPDF.SetColorFill(BaseColor.BLACK);
+            oPDF.SetFontAndSize(bf, 8);
+
+            //Se abre el flujo para escribir el texto
+            oPDF.BeginText();
+            string text = Texto;
+            // Le damos posición y rotación al texto
+            // la posición de Y es al revés de como estamos acostumbrados
+            oPDF.ShowTextAligned(2, text, 100, oSize.Height - 10, 0);
+            oPDF.EndText();
+
+            //crea una nueva pagina y agrega el pdf original
+            PdfImportedPage page = oWriter.GetImportedPage(oReader, 1);
+            oPDF.AddTemplate(page, 0, 0);
+
+            // Cerramos los objetos utilizados
+            oDocument.Close();
+            oFS.Close();
+            oWriter.Close();
+            oReader.Close();
+            //mensaje de guardado
+            MessageBox.Show("Se guardó exitosamente");
+        }
+    }
+}
